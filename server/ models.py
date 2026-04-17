edae49b7e9b2579746e3e366fec08ba6d2b6d5b3
Moringa School <sylvesterkonana005@gmail.com>
2026-04-17 07:44:22 +0300

    Defining Models

--- server/ models.py
+++ server/ models.py
@@ -1,5 +1,42 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
+
+from datetime import date
+
 db = SQLAlchemy()
 
 # Define Models here
+
+class WorkoutExercise(db.Model):
+    __tablename__ = 'workout_exercises'
+    
+    id = db.column(db.Integer, primary_key=True)
+    work_out_id = db.Column(db.Integer, db.ForeignKey('workouts.id'), nullable=False)
+    exercise_id = db.Column(db.Integer, db.ForeignKey('exercises.id'), nullable=False)
+    sets = db.Column(db.Integer,default=0)
+    reps = db.Column(db.Integer,default=0)
+    duration_seconds = db.Column(db.Integer, default=0)
+    
+    
+    
+class Workout(db.Model):
+    __tablename__ = 'workouts'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    duration_minutes = db.Column(db.Integer, nullable=False)
+    notes = db.Column(db.Text) 
+    
+    
+class Exercise(db.Model):
+    __tablename__ = 'exercises'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False, unique=True)
+    category = db.Column(db.String(100), nullable=False)
+    equipment_needed = db.Column(db.Boolean, default=False)
+    
+    
+    
+    
+    
